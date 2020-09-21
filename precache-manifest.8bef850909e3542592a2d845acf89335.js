d025d05281e5a6b555e81b1251a2c0be3249aedd
unknown <ashwinijk18@gmail.com>
2020-09-21 12:47:34 +0530

    Updates

---
+++ precache-manifest.8bef850909e3542592a2d845acf89335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c892f01c428feed33509a5b166900a",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "c9a51c6c76425234360f",
+    "url": "/react_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2802407fb6cdb4dec5c",
+    "url": "/react_deploy/static/js/2.d1b7139d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_deploy/static/js/2.d1b7139d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a51c6c76425234360f",
+    "url": "/react_deploy/static/js/main.999346f0.chunk.js"
+  },
+  {
+    "revision": "ff5b957b5b01ee24d71f",
+    "url": "/react_deploy/static/js/runtime-main.83a01125.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
